bd74e637d7c574ab7e1c250c03de9dbdc9498b89
Samantha Diaz <samidiaz3@gmail.com>
2022-02-01 01:21:32 -0500

    commit tree 575385cdb53eca5481dc386dbd74bacdd72a504d

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+class Polygon {
+
+    constructor(sides) {
+        this.sides = sides
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        return this.sides.reduce(function(acc, side){ return acc + side }, 0)
+    }
+}
+    // get perimeter() {
+    //     if (!Array.isArray(this.sides)) return;
+    //     let sum = 0;
+    //     for (var int of this.sides) {
+    //       sum += int
+    //     }
+    //     return sum
+    //   }
+    // }
+
+class Triangle extends Polygon {
+    get isValid() {
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+
+        return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1)) ? true : false
+
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+        let side4 = this.sides[3]
+
+        return ((side1 === side2) && (side3 === side4) && (side1 === side4)) ? true : false
+    }
+
+    get area(){
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+
+        return side1 * side2
+    }
+}
+
